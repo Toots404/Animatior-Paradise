efd84008e1825410e4bc1a2723f54005149a9492
Toots404 <85807599+Toots404@users.noreply.github.com>
2022-01-17 21:07:08 -1000

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -23,9 +23,32 @@ function getData() {
         childKey = childSnapshot.key;
         Room_names = childKey;
         //Start code
-
+        row =
+          "<div class='room_name' id=" +
+          Room_names +
+          " onclick='redirectToRoomName(this.id)'>#" +
+          Room_names +
+          "</div><hr>";
+        document.getElementById("output").innerHTML += row;
         //End code
       });
     });
 }
 getData();
+
+function addRoom() {
+  room_name = document.getElementById("room_name").value;
+
+  firebase.database().ref("/").child(room.name).update({
+    purpose: "adding room name",
+  });
+
+  localStorage.setItem("room_name", room_name);
+
+  window.location = "kwitter_page.html";
+}
+
+function redirectToRoomName(name) {
+  localStorage.setItem("room_name", name);
+  window.location = "kwitter_page.html";
+}
